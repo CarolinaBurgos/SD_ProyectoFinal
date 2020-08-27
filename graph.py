e9ebeb7a9caa59f35db64e260a456fda4f066492
CarolinaBurgos <caralbur@espol.edu.ec>
2020-08-27 01:02:28 -0500

    Gráficas BoxPlot

--- graph.py
+++ graph.py
@@ -1,19 +1,23 @@
 import matplotlib.pyplot as plt
 
 collect = [[12.53,8.0,23.66]]
+
+fig = plt.figure()
 plt.boxplot(collect, labels=["Original"])
-plt.savefig("original.jpg")
-plt.show()
-plt.cla()
+fig.suptitle('Latencia - Aplicación Original', fontsize=14)
+plt.ylabel("Latencia")
+fig.savefig('original.jpg')
 
 collect = [[8.86,3.32,10.66]]
+fig = plt.figure()
 plt.boxplot(collect, labels=["Mejorado"])
-plt.savefig("mejorado.jpg")
-plt.show()
-plt.cla()
+fig.suptitle('Latencia - Aplicación Actualizada', fontsize=14)
+plt.ylabel("Latencia")
+fig.savefig('mejorado.jpg')
 
 collect = [[12.53,8.0,23.66],[8.86,3.32,10.66]]
+fig = plt.figure()
 plt.boxplot(collect, labels=["Original", "Mejorado"])
-plt.savefig("comparativa.jpg")
-plt.show()
-plt.cla()
+fig.suptitle('Latencia - Comparativa ente aplicaciones', fontsize=14)
+plt.ylabel("Latencia")
+fig.savefig('comparativa.jpg')
